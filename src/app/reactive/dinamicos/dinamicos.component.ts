972d3ecc70a1be80be62025226bf70b319a2089c
David Espinoza <deerhou@gmail.com>
2022-06-09 12:11:48 -0500

    form array

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 interface Persona {
   nombre: string;
@@ -11,15 +17,12 @@ interface Favorito {
   nombre: string;
 }
 
-
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DinamicosComponent implements OnInit {
-
   nuevoJuego: string = '';
 
   persona: Persona = {
@@ -27,56 +30,62 @@ export class DinamicosComponent implements OnInit {
     favoritos: [
       { id: 1, nombre: 'Metal Gear' },
       { id: 2, nombre: 'Death Stranding' },
-    ]
+    ],
+  };
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array(
+      [
+        // ['Metal Gear'], ['Death Stranding']
+      ],
+      Validators.required
+    ),
+  });
+
+  nuevoFavorito: FormControl = this.fb.control('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
+  get favArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
   }
 
-  miFormulario:FormGroup = this.fb.group({
-    nombre: [ '', [Validators.required, Validators.minLength(3)] ],
-    // favoritos: this.fb.array( [
-    //   [ '', [Validators.required, Validators.minLength(3)] ],
-    //   [ '', [Validators.required, Validators.min(0)] ],
-    // ], Validators.required)
-    favoritos: this.fb.array( [
-      ['Metal Gear'],
-      ['Death Stranding'],
-    ], Validators.required)
-  })
+  constructor(private fb: FormBuilder) {}
 
-  
-  get favArr() { return this.miFormulario.get('favoritos') as FormArray }
-    
+  ngOnInit(): void {}
 
-  constructor( private fb:FormBuilder) { }
-
-  ngOnInit(): void {
+  campoesValido(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors && this.miFormulario.touched
+    );
   }
-
-  campoesValido(campo:string) {
-    return this.miFormulario.controls[campo].errors && 
-    this.miFormulario.touched
-  }
-
 
   agregarJuego() {
-    const nuevoFavorito: Favorito = {
-      id: this.persona.favoritos.length + 1,
-      nombre: this.nuevoJuego
+    if (this.nuevoFavorito.invalid) {
+      return;
     }
-
-    this.persona.favoritos.push({ ...nuevoFavorito });
-    this.nuevoJuego = '';
+    this.favArr.push(
+      this.fb.control(this.nuevoFavorito.value, [
+        Validators.required,
+        Validators.minLength(3),
+      ])
+    );
+    this.nuevoFavorito.reset();
   }
 
-  eliminar( index: number ) {
-    this.persona.favoritos.splice(index, 1);
+  eliminar(index: number) {
+    this.favArr.removeAt(index);
+    // this.persona.favoritos.splice(index, 1);
   }
 
   guardar() {
     if (this.miFormulario.invalid) {
-      this.miFormulario.markAllAsTouched()
-      return
+      this.miFormulario.markAllAsTouched();
+      return;
     }
     console.log(this.miFormulario.value);
-    this.miFormulario.reset()
+    // this.miFormulario.reset()
   }
 }
